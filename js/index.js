1ca59d640ec7693105a4804a00eba338aaaa1e3d
Col <codercol@zoho.com>
2018-12-15 00:17:29 +0000

    updates to secure api

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
      randomQuote = quotes[randomNum];
      author = author1[randomNum];*/
 
-     var url= "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+     var url= "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
      $.getJSON(url, function(data){
        $(".quote").html('"'+data.quoteText+ '"');
        $(".author").html("-"+data.quoteAuthor);
